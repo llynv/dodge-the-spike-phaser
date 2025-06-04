8d95ca88ff8fb5a1d9d4a7765baec7d6072e2b30
llynv <linhvg19804@gmail.com>
2025-06-04 15:44:52 +0700

    Improve gh-pages config: add homepage, nojekyll, conditional publicPath

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,56 +2,60 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: './main.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
-    chunkFilename: '[name].[chunkhash].js',
-    clean: true,
-    publicPath: '/dodge-the-spike-phaser/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'ts-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname, 'dist'),
-      },
-      {
-        directory: path.join(__dirname, 'assets'),
-        publicPath: '/assets',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.css'],
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
+module.exports = (env: any, argv: any) => {
+  const isProduction = argv && argv.mode === 'production';
+
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: './main.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[name].[chunkhash].js',
+      clean: true,
+      publicPath: isProduction ? '/dodge-the-spike-phaser/' : '/',
+    },
+    module: {
+      rules: [
         {
-          from: path.resolve(__dirname, 'assets'),
-          to: 'assets',
+          test: /\.ts$/,
+          include: path.resolve(__dirname, 'src'),
+          loader: 'ts-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
       ],
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'index.html'),
-      title: 'Dodge the Spike',
-      inject: 'head',
-    }),
-  ],
+    },
+    devServer: {
+      static: [
+        {
+          directory: path.join(__dirname, 'dist'),
+        },
+        {
+          directory: path.join(__dirname, 'assets'),
+          publicPath: '/assets',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.css'],
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'assets'),
+            to: 'assets',
+          },
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html'),
+        title: 'Dodge the Spike',
+        inject: 'head',
+      }),
+    ],
+  };
 };
